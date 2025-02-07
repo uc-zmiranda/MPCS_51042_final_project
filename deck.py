4022d12b4772b01442a0add4079f6fb6f185a4fb
Zac Miranda <zmiranda@uchicago.edu>
2025-02-07 10:19:22 -0600

    Adding Deck class to project

---
+++ deck.py
@@ -0,0 +1,49 @@
+"""
+This class creates the deck object that will be used when playing the game
+"""
+
+import random as rd
+
+class Deck:
+    def __init__(self):
+        """
+        This class represent the deck of card that will be used
+        when playing the game.
+        """
+        self.reset()
+    
+    def draw(self) -> tuple:
+        """
+        draw a card from the deck and return it to user
+        """
+        return self.cards.pop()
+        
+    def shuffle(self):
+        """
+        Randomizes deck
+        """
+        rd.shuffle(self.cards)
+    
+    def reset(self):
+        """
+        Reset deck to un-shuffled start.
+        """
+        self.cards = self._make_cards()
+    
+    def _make_cards(self):
+        """
+        make cards in deck.
+        """   
+        # setting return list 
+        deck_cards = []
+        
+        # Setting card ranks
+        nums = ['A', 'J', 'Q', 'K']
+        nums.extend([str(x) for x in range(2,11)])
+        
+        # Iterating through suits
+        for suit in ["\u2663", "\u2665", "\u2666", "\u2660"]:
+            for num in nums:
+                deck_cards.append((num,suit))
+        
+        return deck_cards
